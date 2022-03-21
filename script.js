65ab93cd3fd1f042f8dd03133f4653376586e33a
gdelidaki <gdelidaki@gmail.com>
2022-03-21 16:47:49 +0200

    many changes

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // Get the modal
-let modal = document.getElementById("myModal");
+let modal = document.querySelector("#myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.getElementById("image");
-let modalImg = document.getElementById("img01");
-let captionText = document.getElementById("caption");
+let img = document.querySelector("#image");
+let modalImg = document.querySelector("#img01");
+let captionText = document.querySelector("#caption");
 
+//Calculate the surrounding div's width according to the containing image's width
+let imgWidth = img.width + "px";
+let imgDiv = document.querySelector("#img-div");
+imgDiv.style.setProperty('max-width', `calc(${imgWidth} + 2.2rem)`);
+
+//Display modal when image is clicked
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
@@ -15,11 +21,8 @@ img.onclick = function(){
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+const modalDisplay = () => modal.style.display = "none";
 
-modalImg.onclick = function() {
-  modal.style.display = "none";
-}
+// When the user clicks on <span> (x) or the modal image, close the modal
+span.onclick = modalDisplay;
+modalImg.onclick = modalDisplay
